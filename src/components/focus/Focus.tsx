b3dead42d776e4859bb2ccaab0eb35bf2e91b4fd
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-01-08 18:25:07 +0100

    [Added] implementing focus section

---
+++ src/components/focus/Focus.tsx
@@ -0,0 +1,16 @@
+import Title from "../reusable/titles/TItle";
+import FocusContent from "./FocusContent";
+import "./focus.css";
+
+function Focus() {
+  return (
+    <section className="focus">
+      <div className="wrap">
+        <Title className="focus__title">Schwerpunkte</Title>
+        <FocusContent />
+      </div>
+    </section>
+  );
+}
+
+export default Focus;
---
+++ src/components/focus/utils/focusUtils.ts
@@ -0,0 +1,18 @@
+import { FocusType } from "../types/focusTypes";
+
+export const focusUtils: FocusType[] = [
+    { id: 1, text: 'Ängste und Panikattacken' },
+    { id: 2, text: 'Depressionen' },
+    { id: 3, text: 'Chronische Erkrankung' },
+    { id: 4, text: 'Psychosomatische Beschwerden' },
+    { id: 5, text: 'Essstörungen' },
+    { id: 6, text: 'Krisen, traumatische Erlebnisse' },
+    { id: 7, text: 'Überforderung, Burn-out' },
+    { id: 8, text: 'Zwänge' },
+    { id: 9, text: 'Probleme am Arbeitsplatz (Mobbing, Neuorientierung)' },
+    { id: 10, text: 'Beziehungskrisen, Trennungen' },
+    { id: 11, text: 'Sucht' },
+    { id: 12, text: 'Persönlichkeitsentwicklung' },
+    { id: 13, text: 'Schicksalsschläge' },
+    { id: 14, text: 'Kinderwunsch' },
+    { id: 15, text: 'Schlafstörung' }]
