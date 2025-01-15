67c5aa7bdbb80b345b90afee1fe726b74cc1bd4a
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-01-15 17:37:28 +0100

    [Fix] fixing about code structure

--- src/components/about/AboutRowText.tsx
+++ src/components/about/AboutRowText.tsx
@@ -5,10 +5,12 @@ import { aboutUtils } from "./utils/aboutUtils";
 function AboutRowText() {
   return (
     <div className="about__text">
-      <Title className="about__title">Über mich</Title>
-      {aboutUtils.map((item) => (
-        <AboutText key={item.id} item={item} />
-      ))}
+      <div className="about__text-holder">
+        <Title className="about__title">Über mich</Title>
+        {aboutUtils.map((item) => (
+          <AboutText key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
