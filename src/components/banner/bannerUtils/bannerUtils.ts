cb7182f9ea67da2f36b8e646e4eb7318bdf15087
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-02-04 18:34:05 +0100

    [Added] implementing process banner

--- src/components/banner/bannerUtils/bannerUtils.ts
+++ src/components/banner/bannerUtils/bannerUtils.ts
@@ -1,4 +1,5 @@
 export const bannerUtils = [
     { id: 1, page: 'Home', boldedWord: 'Psychotherapie', title: 'Ein Weg zu sich selbst' },
-    { id: 2, page: 'About', boldedWord: 'Was du darüber wissen musst', title: 'Ursula Prinz' }
+    { id: 2, page: 'About', boldedWord: 'Was du darüber wissen musst', },
+    { id: 3, page: 'Process', boldedWord: 'Ablauf ', title: 'und Rahmenbedingungen' }
 ]
--- src/components/pages/ProcessPage.tsx
+++ src/components/pages/ProcessPage.tsx
@@ -1,5 +1,12 @@
+import Banner from "../banner/Banner";
+import ProcessBanner from "../../assets/images/banner/process-banner.png";
+
 function ProcessPage() {
-return 'test';
+  return (
+    <main>
+      <Banner bgImage={ProcessBanner} page="Process" />
+    </main>
+  );
 }
 
 export default ProcessPage;
