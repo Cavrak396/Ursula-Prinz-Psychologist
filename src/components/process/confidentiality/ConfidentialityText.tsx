f60744e5f8db3572a6a6278f24af6e9169c447b1
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-02-05 16:00:06 +0100

    [Added] implementing confidentiality section

---
+++ src/components/process/confidentiality/ConfidentialityText.tsx
@@ -0,0 +1,13 @@
+import Title from "../../reusable/titles/TItle";
+import { ConfidentialityPropType } from "./types/confidentialityTypes";
+
+function ConfidentialityText({ item }: ConfidentialityPropType) {
+  return (
+    <div className="confidentiality__text">
+      <Title className="confidentiality__title">{item.title}</Title>
+      <p className="confidentiality__lines">{item.text}</p>
+    </div>
+  );
+}
+
+export default ConfidentialityText;
---
+++ src/components/process/confidentiality/utils/confidentialityUtils.ts
@@ -0,0 +1,12 @@
+import { ConfidentialityTypes } from "../types/confidentialityTypes";
+
+export const confidentialityUtils: ConfidentialityTypes[] = [{
+    id: 1,
+    title: 'Verschwiegenheitspflicht',
+    text: "Als Psychotherapeutin unterliege ich einer gesetzlich geregelten und strikten Verschwiegenheitspflicht (Psychotherapiegesetz). Das bedeutet, dass sämtliche Inhalte Ihrer Therapie und alle mir anvertrauten oder bekannt gewordenen Informationen vertraulich behandelt werden. Diese Verschwiegenheitspflicht gilt uneingeschränkt – auch gegenüber Ehepartnern, Familienangehörigen, Behörden oder anderen Institutionen."
+},
+{
+    id: 2,
+    title: 'Schweigepflichtentbindung',
+    text: "Die einzige Ausnahme besteht, wenn eine akute Selbst- oder Fremdgefährdung vorliegt. In solchen Fällen wird die weitere Vorgangsweise selbstverständlich zuerst mit Ihnen besprochen. Darüber hinaus können Sie mich in Ausnahmefällen ausdrücklich von der Schweigepflicht entbinden. Die Verschwiegenheitspflicht gilt auch die Supervision, in der Themen aus der Psychotherapie nur in anonymisierter Form besprochen werden."
+}]
