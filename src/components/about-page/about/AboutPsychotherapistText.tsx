aa9ca2ff90d88712650b02d971a5a55eaf677ff9
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-02-12 20:52:08 +0100

    [Fix] fixing about text logic

--- src/components/about-page/about/AboutPsychotherapistText.tsx
+++ src/components/about-page/about/AboutPsychotherapistText.tsx
@@ -1,9 +1,15 @@
-import AboutPsychotherapistTextList from "./AboutPsychotherapistTextList";
-
 function AboutPsychotherapistText() {
   return (
     <div className="about__psychotherapist-text">
-      <AboutPsychotherapistTextList />
+      <p className="about__psychotherapist-text-line">
+        "Ich wurde 1978 in Oberösterreich geboren und lebe nun seit über 2
+        Jahrzehnten in Wien. Nach 17 Jahren Berufserfahrung im Marketing- und
+        Kommunikationsbereich, wechselte ich in den wissenschaftlichen Bereich
+        und arbeite nun seit drei Jahren als wissenschaftliche Mitarbeiterin am
+        Institut für Gesundheitswissenschaften der Ferdinand Porsche FernFH.
+        Zusätzlich arbeite ich an der Psychotherapeutischen Ambulanz (PTA) des
+        ÖAGG als Psychotherapeutin in Ausbildung unter Supervision."`
+      </p>
     </div>
   );
 }
--- src/components/about-page/about/AboutPsychotherapistTextItem.tsx
+++
@@ -1,7 +0,0 @@
-import { AboutPsychotherapistLineType } from "./types/about-page-psychotherapistTypes";
-
-function AboutPsychotherapistTextItem({ item }: AboutPsychotherapistLineType) {
-  return <p className="about__psychotherapist-text-line">{item.text}</p>;
-}
-
-export default AboutPsychotherapistTextItem;
