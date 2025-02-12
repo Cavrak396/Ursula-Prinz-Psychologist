dce310bb44d17cb3de311b4cd188421d36a8c75c
Marko Cavrak <96372327+Cavrak396@users.noreply.github.com>
2025-02-12 21:07:38 +0100

    [Fix] implementing banner text privacy

--- src/components/reusableSections/banner/utils/bannerUtils.ts
+++ src/components/reusableSections/banner/utils/bannerUtils.ts
@@ -5,5 +5,5 @@ export const bannerContent: BannerContentTypes[] = [
     { id: 2, page: 'About', boldedWord: 'Über mich', title: 'Ursula Prinz' },
     { id: 3, page: 'Process', boldedWord: 'Ablauf und', title: 'Rahmenbedingungen' },
     { id: 4, page: 'Imprint', boldedWord: 'Impressum' },
-    { id: 5, page: 'Privacy', boldedWord: 'Datenschutzerklärung' }
+    { id: 5, page: 'Privacy', boldedWord: 'Datenschutz' }
 ]
